b72d729c850367953b8c09cc0b7ff2b5537e1e5c
lihnty <ahmadpondokit@gmail.com>
2024-11-29 06:22:47 +0800

    bagian frontend dari web jwt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './pages/header/Header';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header></Header>
+    <Routes>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Signup></Signup>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+            <Navbar.Brand href="#home"><strong>Not-LoggedIn</strong></Navbar.Brand>
+            <Nav className="ml-auto">
+                <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" className='nav-link'>Signup</Nav.Link>
+            </Nav>
+            </Container>
+        </Navbar>
+    </>
+  )
+}
+
+export default Header;
